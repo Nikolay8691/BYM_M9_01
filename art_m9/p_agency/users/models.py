ee85bba962e2b80f0f41a24386da0402cbd4fea8
Nikolay V <nv-mail@mail.ru>
2022-08-21 12:55:00 +0300

    Profile is added to models.py and admin.py

--- art_m9/p_agency/users/models.py
+++ art_m9/p_agency/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete = models.CASCADE)
+	nick = models.CharField(max_length = 64)
+	phone = models.CharField(max_length = 32)
+
+	def __str__(self):
+		return f' {self.nick} - phone : {self.phone}'
